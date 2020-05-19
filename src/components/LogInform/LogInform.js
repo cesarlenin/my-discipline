967f2d8b656d608ef697b0ff7b344245c2b31294
Cesar Morales <cesarlenin2@live.com>
2020-05-19 13:29:58 -0700

    works on postman

--- src/components/LogInform/LogInform.js
+++ src/components/LogInform/LogInform.js
@@ -1,25 +1,70 @@
 import React, { Component } from 'react'
-import {Link } from 'react-router-dom'
-import './LogInform.css'
+import { Button, Input } from '../Utils/Utils';
+import TokenService from '../../services/token-service';
+import AuthApiService from '../../services/auth-api-service'
 
-export class LogInform extends Component {
+export default class LoginForm extends Component {
+  static defaultProps = {
+    onLoginSuccess: () => {}
+  }
+
+  state = { error: null }
+
+  handleSubmitJwtAuth = ev => {
+    ev.preventDefault()
+    this.setState({ error: null })
+    const { user_name, password } = ev.target
+    
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value,
+    })
+    .then(res => {
+      user_name.value = ''
+      password.value = ''
+      TokenService.saveAuthToken(res.authToken)
+      this.props.onLoginSuccess()
+    })
+    .catch(res => {
+      this.setState({ error: res.error })
+    })
+  }
+
   render() {
-    return ( 
-        <form className='logIn-form'>
-            <div>
-              <label htmlFor="username">Email</label>
-              <input type="text" name='username' id='username' />
-            </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <input type="password" name='password' id='password' />
-            </div>
-            <Link to='/Home'>
-            Log In
-            </Link>
-        </form>
+    const { error } = this.state
+    return (
+      <form
+        className='LoginForm'
+        onSubmit={this.handleSubmitJwtAuth}
+      >
+        <div role='alert'>
+          {error && <p className='red'>{error}</p>}
+        </div>
+        <div className='user_name'>
+          <label htmlFor='LoginForm__user_name'>
+            User name
+          </label>
+          <Input
+            required
+            name='user_name'
+            id='LoginForm__user_name'>
+          </Input>
+        </div>
+        <div className='password'>
+          <label htmlFor='LoginForm__password'>
+            Password
+          </label>
+          <Input
+            required
+            name='password'
+            type='password'
+            id='LoginForm__password'>
+          </Input>
+        </div>
+        <Button type='submit'>
+          Login
+        </Button>
+      </form>
     )
   }
 }
-
-export default LogInform
