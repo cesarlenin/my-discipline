8c65ae84f978b8d6f894b9b2d5745184230333aa
Cesar Morales <cesarlenin2@live.com>
2020-05-29 13:19:09 -0700

    production ready

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'habitToken'
+  API_ENDPOINT: 'https://my-discipline.herokuapp.com/api',
+  TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY
 }
